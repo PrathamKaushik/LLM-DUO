a6059a7a31179f1fd0dd9d6e3cc87c8601f4689f
Pratham Kaushik <prathamkaushik110@gmail.com>
2024-04-11 15:24:04 +0530

    final commit

--- Conversational Q&A Chatbot/app.py
+++ Conversational Q&A Chatbot/app.py
@@ -1,56 +1,43 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv() ## loading all the environment variables
 
-import streamlit as st 
-import os 
-import google.generativeai as genai 
+import streamlit as st
+import os
+import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-
-# Now we will load Gemini Pro model and get response
-model = genai.GenerativeModel("gemini-pro")
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
 chat = model.start_chat(history=[])
 
+
 def get_gemini_response(question):
-    response = chat.send_message(question, stream=True)
-    return response 
+    response=chat.send_message(question,stream=True)
+    return response
 
-# Now we will build  our streamlit app
-st.set_page_config(page_title="Q&A with LLM")
+##initialize our streamlit app
 
-# st.set_page_config(
-#     page_title="Q&A with LLM",
-#     page_icon=":shark:",
-#     layout="centered",  # Can be "wide" or "centered"
-#     initial_sidebar_state="auto",  # Can be "auto", "expanded", "collapsed"
-#     background_image="./Q&A.avif",  # URL or local file path
-# )
+st.set_page_config(page_title="Q&A ChatBot")
 
+st.header("LLM-based Q&A ChatBot")
 
-st.header("LLM-based Q&A Application")
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
 
-# Initializing the session state for chat history if it does not exist
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = [] 
-    
-    
-input = st.text_input("Input:", key="input")
-submit = st.button("Ask me your Question!")
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask the question")
 
 if submit and input:
-    response = get_gemini_response(input)
-    # Adding user query and Bot response to session chat history
-    st.session_state["chat_history"].append(("You", input)) 
-    st.subheader("Here is your Response:")
-    for part in response:
-        st.write(part.text)
-        st.session_state["chat_history"].append(("Bot", part.text)) 
-        
-st.subheader("The Chat History is: ")
-
-for role, text in st.session_state["chat_history"]:
-    st.write(f"{role}:{text}")
+    response=get_gemini_response(input)
+    # Add user query and response to session state chat history
+    st.session_state['chat_history'].append(("You", input))
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+st.subheader("The Chat History is")
     
-
-         
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
